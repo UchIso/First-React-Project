d9573983052652a34724deebf1ac50af25f300ee
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-10-05 16:07:01 +0400

    React Chain

---
+++ src/Login.jsx
@@ -0,0 +1,15 @@
+import { Users } from "./Users"
+
+function Login(){
+    return(
+        <>
+            <div>
+                <p>Enter Email</p>
+                <input type="email"/>
+            </div>
+                <button>Login</button>
+        </>
+    )
+}
+
+export default Login
---
+++ src/Users.jsx
@@ -0,0 +1,7 @@
+export const Users = [
+    "TestMail1@gmail.com",
+    "TestMail2@gmail.com",
+    "TestMail3@gmail.com",
+    "TestMail4@gmail.com",
+    "TestMail5@gmail.com",
+]
