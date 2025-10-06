834a0cd6d0974ba9596118132f63988a37d0a95d
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-10-06 19:12:30 +0400

    OP

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect,useState } from 'react'
+import '../CSS/App.css'
+
+function App() {
+  return (
+    <>
+      <div className='Header'>Header</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '../CSS/index.css'
+import App from './App'
+
+createRoot(document.getElementById('Conteiner')).render(
+    <App />
+)
