04fe13bc129771bf6438cf68c4a46fd3555b8721
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-10-07 07:25:01 +0400

    OP

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import '../CSS/Carts.css'
 
-export default function Cart() {
+export default function Cart({ID,Name,Tag,Price}) {
   return (
-    <div className='Cart'>Cart</div>
+    <div key={ID} className='Cart'>
+      <div className='CartInfo'>
+        <h2>Cart Name: {Name}</h2>
+        <h3>Cart Price: {Price}$</h3>
+        <h5>Cart Tag: #{Tag}</h5>
+      </div>
+    </div>
   )
 }
--- src/Components/Carts.jsx
+++ src/Components/Carts.jsx
@@ -7,6 +7,17 @@ export default function Carts() {
     <div >
         <h1>Carts</h1>
         <div className='Carts'>
+            <Cart ID="1" Name="Deneme" Tag="DenemeTag" Price={2000} />
+            <Cart />
+            <Cart />
+            <Cart />
+            <Cart />
+            <Cart />
+            <Cart />
+            <Cart />
+            <Cart />
+            <Cart />
+            <Cart />
             <Cart />
         </div>
     </div>
