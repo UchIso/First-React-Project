a321b4e378ebcf8cd90fa1109dcb27e1f3d51af8
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-10-07 20:20:01 +0400

    OP

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -6,7 +6,7 @@ export default function Cart({ID,Name,Tag,Price}) {
     <div key={ID} className='Cart'>
       <div className='CartInfo'>
         <h2>Cart Name: {Name}</h2>
-        <h3>Cart Price: {Price}$</h3>
+        {isNaN(Price)? NaN :<h3>Cart Price: {Price}$</h3>}
         <h5>Cart Tag: #{Tag}</h5>
       </div>
     </div>
--- src/Components/Carts.jsx
+++ src/Components/Carts.jsx
@@ -7,18 +7,12 @@ export default function Carts() {
     <div >
         <h1>Carts</h1>
         <div className='Carts'>
-            <Cart ID="1" Name="Deneme" Tag="DenemeTag" Price={2000} />
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
+            <Cart ID={1} Name="Cart-1" Tag="Test-Cart" Price={200} />
+            <Cart ID={2} Name="Cart-2" Tag="Test-Cart" Price={100} />
+            <Cart ID={3} Name="Cart-3" Tag="Test-Cart" Price={300} />
+            <Cart ID={4} Name="Cart-4" Tag="Test-Cart" Price={250} />
+            <Cart ID={5} Name="Cart-5" Tag="Test-Cart" Price={150} />
+            <Cart ID={6} Name="Cart-6" Tag="Test-Cart" Price={350} />
         </div>
     </div>
   )
