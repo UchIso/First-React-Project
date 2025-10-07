26c91c74f2b7673ccec9f3324755a7285bd62f14
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-10-07 20:46:21 +0400

    Products

--- src/Components/Carts.jsx
+++ src/Components/Carts.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import '../CSS/Carts.css'
 import Cart from './Cart'
+import { ProductsData } from './ProductsData'
 
 export default function Carts() {
   return (
     <div >
         <h1>Carts</h1>
         <div className='Carts'>
-            <Cart ID={1} Name="Cart-1" Tag="Test-Cart" Price={200} />
-            <Cart ID={2} Name="Cart-2" Tag="Test-Cart" Price={100} />
-            <Cart ID={3} Name="Cart-3" Tag="Test-Cart" Price={300} />
-            <Cart ID={4} Name="Cart-4" Tag="Test-Cart" Price={250} />
-            <Cart ID={5} Name="Cart-5" Tag="Test-Cart" Price={150} />
-            <Cart ID={6} Name="Cart-6" Tag="Test-Cart" Price={350} />
+          {
+            ProductsData.map((Product)=><Cart ID={Product.ProductID} Name={Product.ProductName} Tag={Product.ProductTag} Price={Product.ProductPrice} />)
+          }
         </div>
     </div>
   )
---
+++ src/Components/ProductsData.jsx
@@ -0,0 +1,50 @@
+export const ProductsData = [
+    {
+        ProductID: 129,
+        ProductName: "Redmi Note 10s",
+        ProductTag: "Phone",
+        ProductPrice: 1200
+    },
+    {
+        ProductID: 9238,
+        ProductName: "Iphone 17",
+        ProductTag: "Phone",
+        ProductPrice: 5500
+    },
+    {
+        ProductID: 8347,
+        ProductName: "Redmi Note 12s",
+        ProductTag: "Phone",
+        ProductPrice: 2500
+    },
+    {
+        ProductID: 1092,
+        ProductName: "LG 1440 Ultra TV",
+        ProductTag: "Tv",
+        ProductPrice: 3500
+    },
+    {
+        ProductID: 2935,
+        ProductName: "Kivi Blender",
+        ProductTag: "Electonic",
+        ProductPrice: 500
+    },
+    {
+        ProductID: 6732,
+        ProductName: "JJK 5",
+        ProductTag: "Book",
+        ProductPrice: 10
+    },
+    {
+        ProductID: 6632,
+        ProductName: "DBZ 1",
+        ProductTag: "Book",
+        ProductPrice: 15
+    },
+    {
+        ProductID: 2935,
+        ProductName: "Bosh Toster",
+        ProductTag: "Electonic",
+        ProductPrice: 100
+    },
+]
